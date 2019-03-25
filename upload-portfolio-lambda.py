b00f9ed6e4259f2f62b0e5a99ebfb161b62f8303
Roberto Hernandez <rh@fusion52.com>
2019-03-25 04:40:14 -0700

    updated lambda function to include codepipeline job, artifact, and location.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,21 +5,46 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:559681517647:deployPortfolioTopic')
+
+    location = {
+      "bucketName": 'portfolio-build.fusion52.com',
+      "objectKey": 'portfolio-build.zip'
+    }
     
-    portfolio_bucket = s3.Bucket('portfolio.fusion52.com')
-    build_bucket = s3.Bucket('portfolio-build.fusion52.com')
+    try:
+        job = event.get('CodePipeline.job')
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+              if artifact['name'] == 'BuildArtifact':
+                location = artifact['location']['s3Location']
+                
+        print ("Building portfolio from " + str(location))        
+      
+        s3 = boto3.resource('s3')
+        
+        portfolio_bucket = s3.Bucket('portfolio.fusion52.com')
+        build_bucket = s3.Bucket(location["bucketName"])
+        
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, 
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        print('Job done!')
+        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully!')
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfolio-build.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-      for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, 
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    
-    print('Job done!')
-    
+    except:
+        topic.publish(Subject='Portfolio Deploy Failed', Message='Portfolio was not deployed successfully!')
+        raise
+      
     return 'Hello from Lambda'
